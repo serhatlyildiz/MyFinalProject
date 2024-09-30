8f896867a46ac38b0b2d6dd406d0b78fbfa29772
Serhat Yıldız <serhat.yildiz2@gazi.edu.tr>
2024-09-30 16:02:15 +0300

    30.09.2024 11. gün yarısı

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        //naming convention
+        //IoC Container -- Inversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //Dependency chain --
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+
+    }
+}
