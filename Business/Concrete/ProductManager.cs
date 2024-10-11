850337cb0ddd237cb885bc428af8e35c1806698a
Serhat Yıldız <serhat.yildiz2@gazi.edu.tr>
2024-10-11 13:43:48 +0300

    20. gün ortası ve son

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -41,7 +41,7 @@ namespace Business.Concrete
         }
 
         //Claim
-        [SecuredOperation("product.add")]
+        //[SecuredOperation("product.add, admin")]
         [ValidationAspect(typeof(ProductValidator))]
         [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
